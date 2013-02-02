e3df95f31897bb445f28317a1ccdf8eb0cc56fb1
Yehor Lvivski <lvivski@gmail.com>
2013-02-02 03:19:48 +0200

    multiple animations

--- js/animato.js
+++ js/animato.js
@@ -23,93 +23,142 @@ World.prototype.update = function update() {
 
 function Item(options) {
   this.dom = options.dom
-  this.state = {
-    translate: [0,0,0],
-    rotate: [0,0,0],
-    scale: [1,1,1]
-  }
   
   this.transform = {
-    translate: [0,0,0],
-    rotate: [0,0,0],
-    scale: [0,0,0]
+    translate: [0, 0, 0],
+    rotate: [0, 0, 0],
+    scale: [1, 1, 1]
   }
   
-  this.animation = {
-    duration: 500,
-    translate: 1,
-    rotate: 0.1,
-    scale: 0.1
-  }
-
-  this.animationStart = this.timestamp = Date.now()
+  this.animations = []
 }
 
-Item.prototype.update = function() {
+Item.prototype.update = function update() {
   var self = this
   this.animate()
-  this.dom.style.webkitTransform = matrix(multiply.apply(null, Object.keys(this.state).map(function(t){ return window[t].apply(null, self.state[t]) })))
+  this.setTransform(matrix(
+      multiply.apply(null,
+        Object.keys(self.transform).map(function (t) {
+          return window[t].apply(null,self.transform[t])
+        })
+      )
+  ))
 }
 
-Item.prototype.setTransform = function(transform) {
+Item.prototype.setTransform = function setTransform(transform) {
   this.dom.style.webkitTransform = transform
 }
 
-Item.prototype.reset = function() {
-  this.setTransform('none')
+Item.prototype.reset = function reset() {
+  if (this.animations.length === 0) return
+  for (var i = 0, len = this.animations.length; i < len; i++) {
+    var a = this.animations[i]
+    a.end()
+  }
+  this.animations = []
 }
 
-Item.prototype.clear = function() {
+Item.prototype.translate = function translate(t) {
+  this.transform.translate[0] += t[0]
+  this.transform.translate[1] += t[1]
+  this.transform.translate[2] += t[2]
+}
+ 
+Item.prototype.rotate = function rotate(r) {
+  this.transform.rotate[0] += r[0]
+  this.transform.rotate[1] += r[1]
+  this.transform.rotate[2] += r[2]
+}
+ 
+Item.prototype.scale = function scale(s) {
+  this.transform.scale[0] += s[0]
+  this.transform.scale[1] += s[1]
+  this.transform.scale[2] += s[2]
+}
+
+
+Item.prototype.clear = function clear() {
   this.transform.translate = [0, 0, 0]
   this.transform.rotate = [0, 0, 0]
   this.transform.scale = [0, 0, 0]
 }
 
-Item.prototype.move = function(m) {
-  this.state.translate[0] += m[0]
-  this.state.translate[1] += m[1]
-  this.state.translate[2] += m[2]
+Item.prototype.animation = function animation(transform, duration, callback) {
+  this.animations.push(new Animation(this, transform, duration, callback))
 }
 
-Item.prototype.rotate = function(r) {
-  this.state.rotate[0] += r[0]
-  this.state.rotate[1] += r[1]
-  this.state.rotate[2] += r[2]
-}
-
-Item.prototype.scale = function(s) {
-  this.state.scale[0] += s[0]
-  this.state.scale[1] += s[1]
-  this.state.scale[2] += S[2]
-}
-
-Item.prototype.animate = function() {
-  var now = Date.now(),
-      diff = now - this.timestamp
-  now - this.start > this.animation.duration && this.clear()
+Item.prototype.animate = function animate() {
+  if (this.animations.length === 0) return
   
-  if (this.transform.translate[0] || this.transform.translate[1] || this.transform.translate[2])
-    this.move([
-      this.transform.translate[0] * this.animation.translate * diff,
-      this.transform.translate[1] * this.animation.translate * diff,
-      this.transform.translate[2] * this.animation.translate * diff
-    ])
+  while (this.animations.length !== 0) {
+    var first = this.animations[0]
+    first.init()
+    if (first.start + first.duration <= Date.now()) {
+      this.animations.shift()
+      first.end()
+      continue
+    }
+    first.run()
+    break
+  }
+}
+
+function Animation(item, transform, duration, callback) {
+  this.item = item
+  this.initial = item.transform
+  
+  this.translate = transform.translate
+  this.rotate = transform.rotate
+  this.scale = transform.scale
+
+  this.start = null
+  
+  this.duration = duration || 0
+
+  this.callback = callback
+}
+
+Animation.prototype.init = function init() {
+  if (this.start !== null) return
+  this.start = Date.now()
+}
+
+Animation.prototype.run = function run() {
+  var percent = (Date.now() - this.start) / this.duration
+
+  if (percent < 0)
+    percent = 0
     
-  if (this.transform.rotate[0] || this.transform.rotate[1] || this.transform.rotate[2])
-    this.rotate([
-      this.transform.rotate[0] * this.animation.rotate * diff,
-      this.transform.rotate[1] * this.animation.rotate * diff,
-      this.transform.rotate[2] * this.animation.rotate * diff
-    ])
-    
-  if (this.transform.scale[0] || this.transform.scale[1] || this.transform.scale[2])
-    this.scale([
-      this.transform.scale[0] * this.animation.scale * diff,
-      this.transform.scale[1] * this.animation.scale * diff,
-      this.transform.scale[2] * this.animation.scale * diff
-    ])
-    
-  this.timestamp = now
+  var transform = this.item.transform,
+      initial = this.initial
+
+  if (this.translate && (this.translate[0] || this.translate[1] || this.translate[2])) {
+    transform.translate = [
+      initial.translate[0] + this.translate[0] * percent,
+      initial.translate[1] + this.translate[1] * percent,
+      initial.translate[2] + this.translate[2] * percent
+    ]
+  }
+  
+  if (this.rotate && (this.rotate[0] || this.rotate[1] || this.rotate[2])) {
+    transform.rotate = [
+      initial.rotate[0] + this.rotate[0] * percent,
+      initial.rotate[1] + this.rotate[1] * percent,
+      initial.rotate[2] + this.rotate[2] * percent
+    ]
+  } 
+  
+  if (this.scale && (this.scale[0] || this.scale[1] || this.scale[2])) {
+    transform.scale = [
+      initial.scale[0] + this.scale[0] * percent,
+      initial.scale[1] + this.scale[1] * percent,
+      initial.scale[2] + this.scale[2] * percent
+    ]
+  }
+}
+
+Animation.prototype.end = function end() {
+  this.callback && this.callback()
 }
 
 var container = new Item({
@@ -130,7 +179,7 @@ var world = new World({});
 
 world.items.push(container)
 
-for (var i = 0; i < 100; i++) {
+for (var i = 0; i < 50; i++) {
   var el = document.createElement('div')
   el.className = 'block'
   var item = new Item({
@@ -138,9 +187,9 @@ for (var i = 0; i < 100; i++) {
   })
   item.update = function(i){
     return function(){
+      return
       var time = Date.now() / 1000;
-      this.dom.style.webkitTransform = matrix(translateZ(Math.cos(5 * (10 * i*i + time)) * 10))
-
+      this.setTransform(matrix(translateZ(Math.cos(5 * (10 * i*i + time)) * 10)))
     }
   }(i)
   world.items.push(item)
@@ -150,34 +199,44 @@ for (var i = 0; i < 100; i++) {
 world.init()
     
 window.addEventListener('keydown', onKeyDown, false)
-window.addEventListener('keyup', function(){container.clear()}, false)
+window.addEventListener('keyup', function(){container.reset()}, false)
 
 function onKeyDown(e) {
-  container.start = Date.now()
+  if (container.animations.length !== 0) return
+  
+  var translate = [0, 0, 0],
+      rotate = [0, 0, 0]
+
   switch(e.keyCode) {
     case keys.UP:
-      container.transform.rotate[0] = 1
+      rotate[0] = 1
       break
     case keys.DOWN:
-      container.transform.rotate[0] = -1
+      rotate[0] = -1
       break
     case keys.LEFT:
-      container.transform.rotate[1] = -1
+      rotate[1] = -1
       break
     case keys.RIGHT:
-      container.transform.rotate[1] = 1
+      rotate[1] = 1
       break
     case keys.w:
-      container.transform.translate[1] = 1
+      translate[1] = 10
       break
     case keys.s:
-      container.transform.translate[1] = -1
+      translate[1] = -10
       break
     case keys.a:
-      container.transform.translate[0] = -1
+      translate[0] = -10
       break
     case keys.d:
-      container.transform.translate[0] = 1
+      translate[0] = 10
       break
   }
+
+  container.animation({
+    translate: translate,
+    rotate: rotate
+  }, 500)
+
 }
