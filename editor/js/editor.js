5c43e4f192344b62732b7361476d45d7148bbbcb
Yehor Lvivski <lvivski@gmail.com>
2013-03-14 18:01:38 +0200

    some initial refactoring

--- editor/js/editor.js
+++ editor/js/editor.js
@@ -2,10 +2,37 @@ var timeline = anima.timeline(),
     item = timeline.add(document.querySelector('.viewport div')),
     keyframes = []
 
+var State = {
+  initial: {
+    translate: [0,0,0],
+    rotate: [0,0,0],
+    scale:[1,1,1]
+  },
+  copy: function (state) {
+    return {
+      translate: state.translate.slice(),
+      rotate: state.rotate.slice(),
+      scale: state.scale.slice()
+    }
+  }
+  diff: function (prev, next) {
+    function differ(type) {
+      return function (t, i) {
+	return t - prev[type][i]
+      }
+    }
+    return {
+      translate: next.translate.map(differ('translate')),
+      rotate: next.rotate.map(differ('rotate')),
+      scale: next.scale.map(differ('scale'))
+    }
+  }
+}
+
 timeline.on('update', function(time) {
   document.querySelector('.timeline input').value = time
 
-  ;['translate','rotate','scale'].forEach(function(t){
+  !['translate','rotate','scale'].forEach(function(t){
     [0,1,2].forEach(function(a) {
       document.querySelector('.controls input[data-transform='+ t +'][data-axis="' + a + '"]').value = item.state[t][a]
     })
@@ -13,46 +40,30 @@ timeline.on('update', function(time) {
 })
 
 function createKeyframe(item, time) {
-  var index = timeline.items.indexOf(item)
+  var index = timeline.items.indexOf(item),
+      state = State.copy(item.state)
+
   keyframes[index] || (keyframes[index] = [])
-  var state = copyState(item.state)
   keyframes[index].push({time: time, state: state})
   keyframes[index] = keyframes[index].sort(function(a, b) { return a.time - b.time })
-  parseKeyframes(item)
-  item.state = copyState(state)
-}
 
-function copyState(state) {
-  return {
-    translate: state.translate.slice(),
-    rotate: state.rotate.slice(),
-    scale: state.scale.slice()
-  }
+  parseKeyframes(item)
+  item.state = State.copy(state)
 }
 
 function parseKeyframes(item) {
   item.stop()
   item.clear()
+
   var index = timeline.items.indexOf(item),
-      prevState = {
-	translate: [0,0,0],
-	rotate: [0,0,0],
-	scale:[1,1,1]
-      },
+      prevState = State.initial,
       prevTime = 0
+
   keyframes[index].forEach(function(frame) {
-    item.animate(diffState(prevState, frame.state), frame.time - prevTime);
-    prevState = copyState(frame.state)
+    item.animate(State.diff(prevState, frame.state), frame.time - prevTime);
+    prevState = State.copy(frame.state)
     prevTime = frame.time
   })
-}
-
-function diffState(prev, next) {
-  return {
-    translate: next.translate.map(function(t, i){ return t - prev.translate[i] }),
-    rotate: next.rotate.map(function(r, i){ return r - prev.rotate[i] }),
-    scale: next.scale.map(function(s, i){ return s - prev.scale[i] })
-  }
 }
 
 document.querySelector('.timeline input[type=range]').addEventListener('change', function () {
