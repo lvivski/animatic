14f0218ede10e2c6a23e5e1a714a13250e317fc5
Yehor Lvivski <lvivski@gmail.com>
2013-03-15 21:55:37 +0200

    editor refactoring

--- editor/js/editor.js
+++ editor/js/editor.js
@@ -1,6 +1,5 @@
-var timeline = anima.timeline(),
-    item = timeline.add($('.viewport div')[0]),
-    keyframes = []
+var timeline = anima.timeline()
+timeline.add($('.viewport div')[0])
 
 function $(selector, context) {
   return Array.prototype.slice.call(
@@ -35,67 +34,83 @@ var State = {
   }
 }
 
-timeline.on('update', function(time) {
-  $('.panel_timeline input[type=range]')[0].value = time
+function Editor(timeline) {
+  this.timeline = timeline
+  this.init()
+}
 
-  !['translate','rotate','scale'].forEach(function(t){
-    [0,1,2].forEach(function(a) {
-      $('.panel_right input[data-transform='+ t +'][data-axis="' + a + '"]')[0].value = item.state[t][a]
+Editor.prototype.init = function () {
+  this.current = 0
+  this.keyframes = []
+
+  var this_ = this
+
+  this.timeline.on('update', function (time) {
+    $('.panel_timeline input[type=range]')[0].value = time
+
+    !['translate','rotate','scale'].forEach(function(t){
+      [0,1,2].forEach(function(a) {
+	$('.panel_right input[data-transform='+ t +'][data-axis="' + a + '"]')[0].value = this_.timeline.items[this_.current].state[t][a]
+      })
     })
-  })
-})
+  }.bind(this))
 
-function createKeyframe(item, time) {
-  var index = timeline.items.indexOf(item),
+  $('.panel_timeline input[type=range]')[0].addEventListener('change', function () {
+    this_.timeline.seek(this.value)
+  }, false)
+
+  $('.panel_timeline input[type=button]')[0].addEventListener('click', function () {
+    this_.keyframe(this_.timeline.currentTime)
+  }, false)
+
+  $('.panel_timeline .code')[0].addEventListener('click', function () {
+    this_.stringify(this_.timeline.items[this_.current])
+  }, false)
+
+  $('.panel_right input[type=range]').forEach(function(range){
+    range.addEventListener('change', function () {
+      this_.timeline.items[this_.current].state[this.dataset['transform']][this.dataset['axis']] = this.value
+    }, false)
+  })
+}
+
+Editor.prototype.keyframe = function (time) {
+  var index = this.current,
+      keyframes = this.keyframes,
+      item = this.timeline.items[this.current],
       state = State.copy(item.state)
 
   keyframes[index] || (keyframes[index] = [])
   keyframes[index].push({time: time, state: state})
   keyframes[index] = keyframes[index].sort(function(a, b) { return a.time - b.time })
 
-  parseKeyframes(item)
+  this.animate(item)
   item.state = State.copy(state)
 }
 
-function parseKeyframes(item) {
+Editor.prototype.animate = function (item) {
   item.stop()
   item.clear()
 
-  var index = timeline.items.indexOf(item),
+  var index = this.current,
       prevState = State.initial,
       prevTime = 0
 
-  keyframes[index].forEach(function(frame) {
+  this.keyframes[index].forEach(function(frame) {
     item.animate(State.diff(prevState, frame.state), frame.time - prevTime)
     prevState = State.copy(frame.state)
     prevTime = frame.time
   })
 }
 
-function getKeyframes(item) {
+Editor.prototype.stringify = function (item) {
   item.clear()
-  popup(item.css(true).keyframes('animation'))
+  this.popup(item.css(true).keyframes('animation'))
 }
 
-function popup(text) {
-  $('.popup')[0].textContent = text.replace(/([;{}])/g, '$1\n')
+Editor.prototype.popup = function (string) {
+  $('.popup')[0].textContent = string.replace(/([;{}])/g, '$1\n')
   $('.popup')[0].style.display = 'block'
 }
 
-$('.panel_timeline input[type=range]')[0].addEventListener('change', function () {
-  timeline.seek(this.value)
-}, false)
-
-$('.panel_timeline input[type=button]')[0].addEventListener('click', function () {
-  createKeyframe(item, timeline.currentTime)
-}, false)
-
-$('.panel_timeline .code')[0].addEventListener('click', function () {
-  getKeyframes(item)
-}, false)
-
-$('.panel_right input[type=range]').forEach(function(range){
-  range.addEventListener('change', function () {
-    item.state[this.dataset['transform']][this.dataset['axis']] = this.value
-  }, false)
-})
+new Editor(timeline)
