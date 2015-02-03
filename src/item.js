4855dfbf78a490549dad4ccccd1a43941190d0d1
Yehor Lvivski <lvivski@gmail.com>
2015-02-03 17:27:48 +0200

    fallback to opacity: 1

--- src/item.js
+++ src/item.js
@@ -22,7 +22,7 @@ Item.prototype.init = function () {
 
 	this.running = true
 
-	var opacity = getComputedStyle(this.dom, null).opacity
+	var opacity = getComputedStyle(this.dom, null).opacity || 1
 
 	this.state = {
 		translate: Vector.zero(),
